b4cebb3c2e88701291d9074f62212f714393fc13
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2021-09-08 21:43:15 +0200

    Add file-name to the upload

--- code/tira-flask-file-upload/util.py
+++ code/tira-flask-file-upload/util.py
@@ -77,7 +77,7 @@ def build_run(data, vm, current_time, input_dataset, run_name):
     with open(os.path.join('/mnt/ceph/tira/model/softwares/scai-qrecc/', vm, 'softwares.prototext'), 'a+') as f:
         f.write('\n\n' + run_software(current_time, input_dataset, next_software_num(vm), run_name))
 
-    with open(os.path.join(run_dir, 'stdout.txt'), 'wb') as f:
+    with open(os.path.join(run_dir, 'stdout.txt'), 'w') as f:
         f.write('This software was not executed in TIRA and documents a manual upload of a run file named "' + run_name + '" on ' + current_time.strftime('%a %b %d %H:%M:%S UTC %Y') + '.\n')
 
     with open(os.path.join(run_dir, 'size.txt'), 'wb') as f:
