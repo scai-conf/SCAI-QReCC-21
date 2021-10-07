4c31b05a934d519587a9f8700c976805243a9609
Johannes Kiesel <johannes.kiesel@uni-weimar.de>
2021-10-07 17:30:01 +0200

    dashes in plot

--- code/evaluation/plot-best.R
+++ code/evaluation/plot-best.R
@@ -4,7 +4,7 @@ data.rewritten <- read.csv("scores-test-dataset-rewritten-2021-07-20.csv")
 data.rewritten.best <- apply(data.rewritten[,3:11], MARGIN=c(2), max, na.rm=TRUE)
 
 mat <- matrix(c(data.best, data.rewritten.best), nrow=2, byrow=TRUE)
-colnames(mat) <- labels(data.best)
+colnames(mat) <- gsub("\\.", "-", labels(data.best))
 
 pdf("plot-best.pdf", width=8, height=4)
 par(mar=c(5,3,0.1,0))
